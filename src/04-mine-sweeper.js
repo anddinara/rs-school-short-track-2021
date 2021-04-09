4d06e45b9f4cec6918e4e1e710d03cac530b646f
anddinara <and.dinara.moscow@gmail.com>
2021-04-09 22:43:17 +0300

    feat: add function for "04-mine-sweeper"

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,44 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrix1 = matrix;
+  let count = 0;
+  for (let i = 0; i < matrix1.length; i++) {
+    for (let j = 0; j < matrix1[i].length; j++) {
+      if (matrix1[i][j] === true) {
+        matrix1[i][j] = 1;
+      } else if (matrix1[i][j] === false) {
+        count = 1;
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix1[i - 1][j - 1] === true) {
+          count += 1;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix1[i - 1][j] === true) {
+          count += 1;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix1[i - 1][j + 1] === true) {
+          count += 1;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && matrix1[i][j - 1] === true) {
+          count += 1;
+        }
+        if (matrix1[i][j + 1] === true) {
+          count += 1;
+        }
+        if (i + 1 < matrix1.length && j + 1 < matrix1[i].length && matrix1[i + 1][j + 1] === true) {
+          count += 1;
+        }
+        if (i + 1 < matrix1.length && j + 1 < matrix1[i].length && matrix1[i + 1][j] === true) {
+          count += 1;
+        }
+        if (i + 1 < matrix1.length && j + 1 < matrix1[i].length && matrix1[i + 1][j + 1] === true) {
+          count += 1;
+        }
+        matrix1[i][j] = count;
+      }
+    }
+  }
+  return matrix1;
 }
 
 module.exports = minesweeper;
