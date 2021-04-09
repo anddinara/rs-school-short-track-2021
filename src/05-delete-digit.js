e7173c27864ca47e8b8d664d52bed54adb287c5d
anddinara <and.dinara.moscow@gmail.com>
2021-04-09 23:08:52 +0300

    feat: add function for "05-delete-digit"

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,31 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const number = n;
+  const arr2 = number.toString().split('');
+
+  const arr1 = number.toString().split('');
+
+  for (let i = 1; i < arr1.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if (+arr1[i] < +arr1[j]) {
+        const min = +arr1[i];
+        arr1[i] = +arr1[j];
+        arr1[j] = min;
+      }
+    }
+  }
+
+  const element = String(arr1[0]);
+
+  if (arr2.indexOf(element) > -1) {
+    arr2.splice(arr2.indexOf(element), 1);
+  }
+
+  const total = Number(arr2.join(''));
+
+  return total;
 }
 
 module.exports = deleteDigit;
